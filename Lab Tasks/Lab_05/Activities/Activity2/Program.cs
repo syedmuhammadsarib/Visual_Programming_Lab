1b856524982a376293de3115b9324d907707eab7
Syed Muhammad Sarib <121973086+syedmuhammadsarib@users.noreply.github.com>
2024-10-16 21:31:32 +0500

    Add files via upload

---
+++ Lab Tasks/Lab_05/Activities/Activity2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        testCatch2();
+    }
+
+    private static void testCatch2()
+    {
+        System.IO.StreamWriter ew = null;
+        try
+        {
+            ew = new System.IO.StreamWriter(@"D:\text.txt");
+            ew.WriteLine("Hello");
+        }
+        catch (System.IO.FileNotFoundException ex)
+        {
+            System.Console.WriteLine(ex.ToString());
+        }
+        catch (System.IO.IOException ex)
+        {
+            System.Console.WriteLine(ex.ToString());
+        }
+        finally
+        {
+            ew.Close();
+        }
+        System.Console.WriteLine(" Done");
+    }
+}
+
