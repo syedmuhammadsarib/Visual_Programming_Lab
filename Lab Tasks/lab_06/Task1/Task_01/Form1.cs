3bb3c1193e1ee07fa2d23b929f482593e9bdc5cf
Syed Muhammad Sarib <121973086+syedmuhammadsarib@users.noreply.github.com>
2024-11-13 23:43:17 +0500

    Add files via upload

---
+++ Lab Tasks/lab_06/Task1/Task_01/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+                    }
+
+
+        private bool check_input()
+        {
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("Enter First Number", "Input Required");
+                return false;
+            }
+            else if (string.IsNullOrEmpty(textBox2.Text))
+            {
+                MessageBox.Show("Enter Second Number", "Input Required");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(check_input())
+            {
+                int value1 = int.Parse(textBox1.Text);
+                int value2 = int.Parse(textBox2.Text);
+
+                label3.Visible = true;
+                label4.Visible = true;
+                label4.Text = (value1 + value2).ToString();
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label3.Visible = false;
+            label4.Visible = false;
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            if (check_input())
+            {
+                int value1 = int.Parse(textBox1.Text);
+                int value2 = int.Parse(textBox2.Text);
+
+                label3.Visible = true;
+                label4.Visible = true;
+                label4.Text = (value1 - value2).ToString();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (check_input())
+            {
+                int value1 = int.Parse(textBox1.Text);
+                int value2 = int.Parse(textBox2.Text);
+
+                label3.Visible = true;
+                label4.Visible = true;
+                label4.Text = (value1 * value2).ToString();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (check_input())
+            {
+                int value1 = int.Parse(textBox1.Text);
+                int value2 = int.Parse(textBox2.Text);
+                if(value2 == 0)
+                {
+                    MessageBox.Show("Enter Correct Value of Second ");
+                    textBox2.Clear();
+                    textBox2.Focus();
+                }
+                else
+                {
+                    label3.Visible = true;
+                    label4.Visible = true;
+                    label4.Text = (value1 / value2).ToString();
+                }
+                
+            }
+        }
+    }
+}
---
+++ Lab Tasks/lab_06/Task2/Task_02/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_02
+{
+    public partial class Form1 : Form
+    {
+
+        private double value1=0;
+        private double value2=0;
+        private string operator_;
+        bool check = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox1.Text))
+            {
+                value1 = double.Parse(textBox1.Text);
+                textBox1.Clear();
+                textBox1.Focus();
+                operator_ = "+";
+            }
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox1.Text))
+            {
+                value1 = double.Parse(textBox1.Text);
+                textBox1.Clear();
+                textBox1.Focus();
+                operator_ = "-";
+            }
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox1.Text))
+            {
+                value1 = double.Parse(textBox1.Text);
+                textBox1.Clear();
+                textBox1.Focus();
+                operator_ = "*";
+            }
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox1.Text))
+            {
+                value1 = double.Parse(textBox1.Text);
+                textBox1.Clear();
+                textBox1.Focus();
+                operator_ = "/";
+            }
+        }
+
+        private bool check_value()
+        {
+            if(value1 == 0  &&  value2==0 )
+            {
+                return false;
+            }   
+            else
+            {
+                return true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(check)
+            {
+                textBox1.Clear();
+                check = false;
+            }
+            textBox1.Text += "1";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (check)
+            {
+                textBox1.Clear();
+                check = false;
+            }
+            textBox1.Text += "2";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (check)
+            {
+                textBox1.Clear();
+                check = false;
+            }
+            textBox1.Text += "3";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (check)
+            {
+                textBox1.Clear();
+                check = false;
+            }
+            textBox1.Text += "4";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (check)
+            {
+                textBox1.Clear();
+                check = false;
+            }
+            textBox1.Text += "5";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (check)
+            {
+                textBox1.Clear();
+                check = false;
+            }
+            textBox1.Text += "6";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (check)
+            {
+                textBox1.Clear();
+                check = false;
+            }
+            textBox1.Text += "7";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (check)
+            {
+                textBox1.Clear();
+                check = false;
+            }
+            textBox1.Text += "8";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (check)
+            {
+                textBox1.Clear();
+                check = false;
+            }
+            textBox1.Text += "9";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            if (check)
+            {
+                textBox1.Clear();
+                check = false;
+            }
+            textBox1.Text += "0";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            if (check)
+            {
+                textBox1.Clear();
+                check = false;
+            }
+            textBox1.Text += ".";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            value2 = int.Parse(textBox1.Text);
+            if(operator_ == "+")
+            {
+                textBox1.Text = (value1 + value2).ToString();
+            }
+            if (operator_ == "-")
+            {
+                textBox1.Text = (value1 - value2).ToString();
+            }
+            if (operator_ == "*")
+            {
+                textBox1.Text = (value1 * value2).ToString();
+            }
+            if (operator_ == "/")
+            {
+                textBox1.Text = (value1 / value2).ToString();
+            }
+            value1 = 0;
+            value2 = 0;
+            operator_ = "";
+            check = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
