0cc83b4f50be3beb5e0ea298e22ec058cab94cfc
Syed Muhammad Sarib <121973086+syedmuhammadsarib@users.noreply.github.com>
2024-11-27 22:47:52 +0500

    Add files via upload

---
+++ Lab Tasks/Lab_08 new/Lab_08/CustomerValidation.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Activity_01_Lab
+{
+    internal class CustomerValidation
+    {
+        public void checkCustomerName(string name)
+        {
+            if(name.Length > 10)
+            {
+                throw new Exception("Name should be within 10 characters.");
+            }
+            else if(string.IsNullOrEmpty(name))
+            {
+                throw new Exception("Name is Required");
+            }
+        }
+
+        public void checkGender(RadioButton male,RadioButton female)
+        {
+            if (!male.Checked && !female.Checked)
+            {
+                throw new Exception("Select your Gender");
+            }
+            
+        }
+
+        public void checkHobby(CheckBox one, CheckBox two)
+        {
+            if (!one.Checked && !two.Checked)
+            {
+                throw new Exception("Select your Hobby");
+            }
+
+        }
+
+        public void checkStatus(RadioButton m, RadioButton um)
+        {
+            if (!m.Checked && !um.Checked)
+            {
+                throw new Exception("Select your Status");
+            }
+
+        }
+
+    }
+}
---
+++ Lab Tasks/Lab_08 new/Lab_08/frmCustomerPreview.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity_01_Lab
+{
+    public partial class frmCustomerPreview : Form
+    {
+        public frmCustomerPreview()
+        {
+            InitializeComponent();
+        }
+
+        private void frmCustomerPreview_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void SetValues(string name, string country, string gender, string hobby, string status)
+        {
+            lblName.Text = name;
+            lblCountry.Text = country;
+            lblGender.Text = gender;
+            lblHobby.Text = hobby;
+            lblStatus.Text = status;
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
