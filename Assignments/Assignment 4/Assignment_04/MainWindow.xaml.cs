b76c224a445f8aad012552cdad486bb3abe93dab
Syed Muhammad Sarib <121973086+syedmuhammadsarib@users.noreply.github.com>
2024-12-09 23:17:50 +0500

    Add files via upload

---
+++ Assignments/Assignment 4/Assignment_04/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿    using System.Collections.ObjectModel;
+    using System.Numerics;
+    using System.Windows;
+
+    namespace CricketTeamManager
+    {
+        public class Player
+        {
+            public int PlayerID { get; set; }
+            public string Name { get; set; }
+        }
+
+        public partial class MainWindow : Window
+        {
+            public ObservableCollection<Player> Players { get; set; }
+
+            public MainWindow()
+            {
+                InitializeComponent();
+                Players = new ObservableCollection<Player>();
+                PlayersListBox.ItemsSource = Players;
+            }
+
+            private void AddPlayer_Click(object sender, RoutedEventArgs e)
+            {
+                if (!string.IsNullOrWhiteSpace(PlayerNameTextBox.Text))
+                {
+                    int newId = Players.Count + 1;
+                    Players.Add(new Player { PlayerID = newId, Name = PlayerNameTextBox.Text });
+                    PlayerNameTextBox.Clear();
+                }
+            }
+
+            private void RemovePlayer_Click(object sender, RoutedEventArgs e)
+            {
+                if (PlayersListBox.SelectedItem is Player selectedPlayer)
+                {
+                    Players.Remove(selectedPlayer);
+                }
+            }
+
+            private void PlayerNameTextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+            {
+                // Handle text changed event if needed
+            }
+        }
+    }
