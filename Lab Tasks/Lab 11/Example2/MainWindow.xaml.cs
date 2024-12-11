df47c18a08304cd7fdbd708f2264a3661494d970
Syed Muhammad Sarib <121973086+syedmuhammadsarib@users.noreply.github.com>
2024-12-11 14:52:55 +0500

    Add files via upload

---
+++ Lab Tasks/Lab 11/Example2/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Example2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+        private void HandleCheck(object sender, RoutedEventArgs e)
+        {
+            CheckBox cb = sender as CheckBox;
+            if(cb.Name=="checkbox1")
+            {
+                textbox1.Text = "2 State Checked is checked";
+            }
+            else
+            {
+                textbox2.Text = "3 State Checked is checked";
+            }
+        }
+
+        private void HandleUnchecked(object sender, RoutedEventArgs e)
+        {
+            CheckBox cb = sender as CheckBox;
+            if (cb.Name == "checkbox1")
+            {
+                textbox1.Text = "2 State Checked is Unchecked";
+            }
+            else
+            {
+                textbox2.Text = "3 State Checked is Unchecked";
+            }
+        }
+
+        private void HandleThirdState(object sender, RoutedEventArgs e)
+        {
+            CheckBox cb = sender as CheckBox;
+            
+                textbox2.Text = "3 State Checked is Indeterminate state";
+            
+            
+        }
+    }
+
+
+}
