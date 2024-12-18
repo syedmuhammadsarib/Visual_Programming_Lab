b524827fea750b1e96c89e68f2deb5c28bb9044c
Syed Muhammad Sarib <121973086+syedmuhammadsarib@users.noreply.github.com>
2024-12-18 13:41:57 +0500

    Add files via upload

---
+++ Lab Tasks/Lab 12/Activity/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows;
+
+namespace Activity
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            
+            var timer = new System.Windows.Threading.DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += (s, e) => CurrentTime = DateTime.Now;
+            timer.Start();
+        }
+
+        public static readonly DependencyProperty CurrentTimeProperty = DependencyProperty.Register(
+            "CurrentTime",
+            typeof(DateTime),
+            typeof(MainWindow),
+            new FrameworkPropertyMetadata(DateTime.Now, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, OnCurrentTimeChanged)
+        );
+
+        public DateTime CurrentTime
+        {
+            get { return (DateTime)GetValue(CurrentTimeProperty); }
+            set { SetValue(CurrentTimeProperty, value); }
+        }
+
+        private static void OnCurrentTimeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            
+        }
+    }
+}
