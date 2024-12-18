2e38a0f51414d32c5a6e79ce69d9114f029b53bd
Syed Muhammad Sarib <121973086+syedmuhammadsarib@users.noreply.github.com>
2024-12-18 13:47:05 +0500

    Add files via upload

---
+++ Lab Tasks/Lab 12/Activity2/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Three parts of LINQ query:
+
+            //1. Data source
+            int[] numbers = {0, 1, 2, 3, 4, 5 ,6};
+
+            //2. Query creation
+            var numQuery = from num in numbers
+                           where (num % 2) == 0
+                           select num;
+
+            //3. Query execution
+            foreach (int num in numQuery)
+            {
+                Console.WriteLine("{0,1}", num);
+            }
+        }
+    }
+}
+  
