9f5b22c10aadfc5f8d3457162015350a92e279d5
Syed Muhammad Sarib <121973086+syedmuhammadsarib@users.noreply.github.com>
2025-01-01 19:13:10 +0500

    Add files via upload

---
+++ Lab Tasks/Lab 13/Task2 Updated/Components/Models/CityRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace Task2.Components.Models
+{
+    public class CityRepository
+    {
+        public static List<string> cities = new List<string>()
+        {
+            "Toronto",
+            "Montreal",
+            "Ottawa",
+            "Calgary",
+            "Halifax"
+        };
+        public static List<string> GetCities() => cities;
+    }
+}
